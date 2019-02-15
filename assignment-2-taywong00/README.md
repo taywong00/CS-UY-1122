94b43b682deb23452e2fba7a050f936e836a1e22
Taylor Wong <taywong00@gmail.com>
2019-02-15 07:37:00 -0800

    updated 2/15/19

---
+++ assignment-2-taywong00/README.md
@@ -0,0 +1,18 @@
+# Assignment 2
+
+Edit this file to complete the following information below:
+
+* Name: 
+* Net ID:
+
+## Description
+
+Implement one of the following sorting algorithms on the data provided in data.txt.
+
+1. Bubble Sort
+2. Selection Sort
+3. Quicksort
+
+Your code must be in sort.py
+
+You may not use any external libraries. This is an individual assignment.
---
+++ assignment-2-taywong00/sort.py
@@ -0,0 +1,17 @@
+# Assignment 2
+# Name: Taylor Wong
+# Net ID: tw1835
+
+# Your code goes here
+# Sorting algorithm implemented: Insertion Sort
+
+def insertion_sort(sequence):
+    # parse over items
+    for partition in range(1, len(seq)): # the first elem is already sorted
+        for next_item in range(partition, 0, -1): # insert the next item into its sorted place behind the partition
+            if sequence[j] < sequence[j-1]: # swap the items behind partition to sort
+                sequence[j-1]
+                sequence[j] = seq[j]
+                sequence[j-1]
+            else:
+                break
